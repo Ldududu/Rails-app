d68fccd8e267d074a016719b0325b17481d0e5d7
Leo Takaoka <tlkanb0908@gmail.com>
2020-04-21 21:52:22 +0900

    usersコントローラの追加,signupページ追加

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -7,5 +7,6 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_template 'home/top'
     assert_select "a[href=?]", root_path
     assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", signup_path
   end
 end
