c88346aa9d15a2fd9172de4b860439c03b868b8f
Leo Takaoka <tlkanb0908@gmail.com>
2020-04-23 19:10:01 +0900

    編集ページのbefore action実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -29,4 +31,42 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
+
+  def update
+    @user = User.find(params[:id])
+    @user.name = params[:name]
+    @user.email = params[:email]
+    @user.password = params[:password]
+    @user.password_confirmation = params[:password_confirmation]
+
+    if params[:image_name]
+      @user.image_name = "#{@user.id}.png"
+      image = params[:image_name]
+      File.binwrite("public/user_images/#{@user.image_name}", image.read)
+    end
+
+    if @user.save
+      flash[:notice] = "ユーザー情報を編集しました"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+    #beforeアクション
+    
+    #ログイン済みユーザーか確認
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please Log in"
+        redirect_to login_url
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless @user == current_user
+      end
 end
