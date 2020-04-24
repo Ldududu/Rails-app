aeb7a44d8855539ec76c5de8a74ea7a6f0e8393d
Leo Takaoka <tlkanb0908@gmail.com>
2020-04-24 14:14:57 +0900

    画像投稿機能

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,9 @@
 class PostsController < ApplicationController
   before_action :logged_in_user, only: [:index, :new, :create, :destroy]
+  before_action :correct_user,   only: :destroy
 
   def index
+    @posts = Post.paginate(page: params[:page], per_page: 5)
   end
 
   def new
@@ -12,19 +14,26 @@ class PostsController < ApplicationController
     @post = current_user.posts.build(post_params)
     if @post.save
       flash[:success] = "投稿しました"
-      redirect_to root_url
+      redirect_to posts_url
     else
       render 'new'
     end
   end
 
   def destroy
-
+    @post.destroy
+    flash[:success] = "削除しました"
+    redirect_to request.referrer || root_url
   end
 
   private
 
     def post_params
-      params.require(:post).permit(:title, :content)
+      params.require(:post).permit(:title, :content, :picture)
+    end
+
+    def correct_user
+      @post = current_user.posts.find_by(id: params[:id])
+      redirect_to posts_url if @post.nil?
     end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,18 @@
 class Post < ApplicationRecord
   belongs_to :user
   default_scope -> { order(created_at: :desc) }
+  mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
-  validates :content, presence: true, length: { maximum: 400 }
+  validates :content, presence: true, length: { maximum: 400, minimum: 200 }
   validates :title, presence: true, length: { maximum: 40 }
+  validate :picture_size
+
+  private
+
+    # アップロードされた画像のサイズをバリデーションする
+    def picture_size
+      if picture.size > 5.megabytes
+        errors.add(:picture, "5MB以下の画像にしてください")
+      end
+    end
 end
